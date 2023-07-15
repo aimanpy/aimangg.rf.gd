86524dcadd98ce785aa8208178c5c7f4691236d1
aiman.py <139182524+aimanpy@users.noreply.github.com>
2023-07-15 19:20:52 -0300

    Added favicon animation

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
 var soundPlayed = false;
-var audio = new Audio('micancion.mp3');
-audio.loop = true; // Reproducir en bucle
 
 function playSound() {
     if (!soundPlayed) {
+        var audio = new Audio('micancion.mp3');
         audio.play();
         soundPlayed = true;
     }
 }
 
-// Evento 'ended' para reiniciar la reproducción
-audio.addEventListener('ended', function() {
-    audio.currentTime = 0; // Reiniciar al principio del audio
-    audio.play();
-});
-
 function handleClick() {
     playSound();
     document.getElementById('click-text').remove();
     document.getElementById('overlay').classList.add('visible');
-    document.body.style.backgroundColor = getRandomColor();
-}
-
-function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -38,3 +21,33 @@ document.addEventListener('DOMContentLoaded', function() {
     
     clickText.addEventListener('click', handleClick);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var customCursor = document.getElementById('custom-cursor');
+    
+    // Seguir el cursor
+    document.addEventListener('mousemove', function(event) {
+        customCursor.style.left = event.clientX + 'px';
+        customCursor.style.top = event.clientY + 'px';
+    });
+});
+
+var faviconImages = [
+    'favicon1.jpg',
+    'favicon2.jpg',
+    'favicon3.jpg'
+];
+
+var faviconIndex = 0;
+
+function changeFavicon() {
+    var favicon = document.getElementById('favicon');
+    favicon.href = faviconImages[faviconIndex];
+    
+    faviconIndex++;
+    if (faviconIndex >= faviconImages.length) {
+        faviconIndex = 0;
+    }
+}
+
+setInterval(changeFavicon, 1000);
